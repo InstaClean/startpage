a05357aac3a5788ddda3e5aba577c91940e46093
Kevin <kbeens@protonmail.com>
2021-09-18 09:29:44 -0700

    fixed image plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,13 +13,12 @@ module.exports = {
       failOnError: true,
       // deprecated options and their defaults:
       base64Width: 20,
-      forceBase64Format: ``, // valid formats: png,jpg,webp
+      forceBase64Format: `png`, // valid formats: png,jpg,webp
       useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
       stripMetadata: true,
       defaultQuality: 50,   
       }
     },
     "gatsby-plugin-image",
-    "gatsvy-transformer-sharp"
   ],
 };
